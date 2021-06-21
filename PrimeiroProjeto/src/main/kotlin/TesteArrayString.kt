c4d15069c229cecbcec2023b02d6a535b454f866
Ogami_Raufa <brokenzero67@gmail.com>
2021-06-21 20:50:07 -0300

    Funcões de busca
    Uso de funcões de busca nas coleções.

--- PrimeiroProjeto/src/main/kotlin/TesteArrayString.kt
+++ PrimeiroProjeto/src/main/kotlin/TesteArrayString.kt
@@ -1,18 +1,21 @@
+
+
 fun main(){
 
-    val nomes= Array(3){""}
-    nomes[0] = "Manoela"
-    nomes[1] = "Janaina"
-    nomes[2] = "Erlane"
 
-    for(nome in nomes){
+    val salario = DoubleArray(3)
 
-        println(nome)
-    }
+    salario[0] = readLine()!!.toDouble()
+    salario[1] = readLine()!!.toDouble()
+    salario[2] = readLine()!!.toDouble()
 
-    println("----------------------------------*****")
-    nomes[2]="Emilly"
-    nomes.sort()
-    nomes.forEach {println(it)}
+    println("O salario inserido no indice 1 foi: ${salario[0]}")
+    println("O salario inserido no indice 2 foi: ${salario[1]}")
+    println("O salario inserido no indice 3 foi: ${salario[2]}")
+
+
+    println(salario.count{it > 2000.0})
+    println(salario.find{it == 2000.0})
+
 
 }
